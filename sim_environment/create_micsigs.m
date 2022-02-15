074b212beb3b41e01e2a4ddcad829b0655dda0b5
YZ <charliehe0211@gmail.com>
2022-02-16 00:12:09 +0100

    TODA white noise esti

--- sim_environment/create_micsigs.m
+++ sim_environment/create_micsigs.m
@@ -1,14 +1,14 @@
 function mic = create_micsigs(num_mics, speech_files, noise_files, duration)
     
     load("Computed_RIRs.mat");
-    
+
     Fs = fs_RIR;
     sig_len = duration * Fs;
     
     %% create the array for audio
     speech_wav = zeros(sig_len, length(speech_files));
     noise_wav = zeros(sig_len, length(noise_files));
-    
+
     %% create the array for recorded signal
     speech_rec = zeros(sig_len, num_mics);
     noise_rec = zeros(sig_len, num_mics);
@@ -20,13 +20,13 @@ function mic = create_micsigs(num_mics, speech_files, noise_files, duration)
         
         %% resample if needed
         if(f_ss ~= Fs)
-            
+            speech = resample(speech, Fs, f_ss);
         end
         
         speech_wav(:, i) = speech;
         
         %% pass through the channel
-        speech_rec = speech_rec + fftfilt(RIR_sources, speech_wav(:, i));
+        speech_rec = speech_rec + fftfilt(RIR_sources(:, :, i), speech_wav(:, i));
     end
     
     for i=1:length(noise_files)
@@ -36,13 +36,13 @@ function mic = create_micsigs(num_mics, speech_files, noise_files, duration)
         
         %% resample if needed
         if(f_sn ~= Fs)
-            
+            noise = resample(noise, Fs, f_sn);
         end
         
         noise_wav(:, i) = noise;
         
         %% pass through the channel
-        noise_rec = noise_rec + fftfilt(RIR_noise, noise_wav(:, i));
+        noise_rec = noise_rec + fftfilt(RIR_noise(:, :, i), noise_wav(:, i));
     end
     
     %% add noise to speech
--- sim_environment/main.m
+++ sim_environment/main.m
@@ -1,8 +1,14 @@
 clear;
 
-speech_files = ["speech1.wav", "speech2.wav"];
-noise_files = ["Babble_noise1.wav"];
+speech_files = ["speech1.wav"];
+noise_files = [];
 
-mic = create_micsigs(3, speech_files, noise_files, 3);
+mic = create_micsigs(2, speech_files, noise_files, 5);
 
-soundsc(mic(:, 1), 44100);
+figure
+plot(mic(:, 1), 'DisplayName', 'Mic 1');
+hold on
+plot(mic(:, 2), 'DisplayName', 'Mic 2');
+legend
+
+% soundsc(mic, 44100);
