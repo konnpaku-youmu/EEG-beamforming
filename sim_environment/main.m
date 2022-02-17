4b352b8e3b5f92b7b7990b344d61a01172e8e0ea
YZ <charliehe0211@gmail.com>
2022-02-17 23:03:03 +0100

    TDOA speech

--- sim_environment/main.m
+++ sim_environment/main.m
@@ -1,14 +1,25 @@
+% %% Part 2: Microphone signals
+% 
+% clear;
+% 
+% speech_files = ["speech1.wav"];
+% noise_files = [];
+% 
+% mic = create_micsigs(2, speech_files, noise_files, 5);
+% 
+% figure
+% plot(mic(:, 1), 'DisplayName', 'Mic 1');
+% hold on
+% plot(mic(:, 2), 'DisplayName', 'Mic 2');
+% legend
+
+%% Part 3: TDOA
+
 clear;
 
-speech_files = ["speech1.wav"];
+speech_files = ["part1_track1_dry.wav"];
 noise_files = [];
 
-mic = create_micsigs(2, speech_files, noise_files, 5);
+mic = create_micsigs(2, speech_files, noise_files, 10);
 
-figure
-plot(mic(:, 1), 'DisplayName', 'Mic 1');
-hold on
-plot(mic(:, 2), 'DisplayName', 'Mic 2');
-legend
-
-% soundsc(mic, 44100);
+t_diff = TDOA_corr(mic);
