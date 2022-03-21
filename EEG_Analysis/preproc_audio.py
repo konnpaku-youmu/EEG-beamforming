bc07920ecc58bb73503ea494dbd688303fbb8f22
YZ <charliehe0211@gmail.com>
2022-03-21 23:24:08 +0100

    audio & eeg pre-proc

--- EEG_Analysis/preproc_audio.py
+++ EEG_Analysis/preproc_audio.py
@@ -1,13 +1,12 @@
 import psutil
 import glob
 import numpy as np
-import matplotlib.pyplot as plt
 
-from concurrent.futures import ProcessPoolExecutor
+import concurrent.futures
 
 import soundfile
 import scipy.io as sio
-from scipy.signal import resample, fftconvolve
+from scipy.signal import resample, fftconvolve, butter, sosfilt
 
 
 def load_audio_filenames(path):
@@ -15,18 +14,18 @@ def load_audio_filenames(path):
     Loads audio filenames from a path.
     """
     print("Loading audio filenames...")
-    filenames = glob.glob(path+"/*.wav")
+    filenames = glob.glob(path + "/*.wav")
     filenames.sort()
-    print('Found {} wav files'.format(len(filenames)))
+    print("Found {} wav files".format(len(filenames)))
     return filenames
 
 
 def load_preproc_const():
     audio_preproc_cnst = sio.loadmat("audioprepro_constants.mat")
-    g = audio_preproc_cnst['g']
+    g = audio_preproc_cnst["g"]
     H = []
     for i in range(g.shape[0]):
-        h = g[i, 0][0, 0]['h']
+        h = g[i, 0][0, 0]["h"]
         H.append(h)
     return H
 
@@ -46,13 +45,15 @@ def load_audio_and_proc(fs_r, fs_linreg, fs_cnn, H, n_subbands, wav_file):
         h = H[sub_band][:, 0]
 
         # Filter audio data
-        subband_filt = fftconvolve(resample_audio, h, mode='same')
+        subband_filt = fftconvolve(resample_audio, h, mode="same")
         # Power compression
+        neg_idx = np.where(subband_filt < 0)
         subband_filt = np.power(np.abs(subband_filt), 0.6)
+        subband_filt[neg_idx] = -subband_filt[neg_idx]
 
         audio_filt[sub_band, :] = subband_filt
 
-        # Downsample filtered signal to 20Hz & 70Hz
+    # Downsample filtered signal to 20Hz & 70Hz
     audio_linreg = resample(
         audio_filt, audio_filt.shape[1] // fs_r * fs_linreg, axis=1)
     audio_cnn = resample(
@@ -62,8 +63,18 @@ def load_audio_and_proc(fs_r, fs_linreg, fs_cnn, H, n_subbands, wav_file):
     audio_linreg = np.sum(audio_linreg, axis=0)
     audio_cnn = np.sum(audio_cnn, axis=0)
 
+    # Bandpass filter: 4-th butterworth filter
+    sos_linreg = butter(4, [1, 9], btype="bandpass",
+                        fs=fs_linreg, output="sos")
 
-if __name__ == '__main__':
+    sos_cnn = butter(4, [1, 32], btype="bandpass", fs=fs_cnn, output="sos")
+    audio_linreg_bp = sosfilt(sos_linreg, audio_linreg)
+    audio_cnn_bp = sosfilt(sos_cnn, audio_cnn)
+
+    return {"linreg": audio_linreg_bp, "cnn": audio_cnn_bp}
+
+
+if __name__ == "__main__":
     # Load audio filenames
     wav_files = load_audio_filenames("./dataset/Audio")
 
@@ -75,7 +86,25 @@ if __name__ == '__main__':
     H = load_preproc_const()
     n_subbands = len(H)
 
-    with ProcessPoolExecutor(max_workers=psutil.cpu_count() // 2) as executor:
-        for wav_file in wav_files:
-            executor.submit(load_audio_and_proc, fs_r, fs_linreg,
-                            fs_cnn, H, n_subbands, wav_file)
+    audio_data = {}
+    # Load audio data and process
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=psutil.cpu_count() // 2
+    ) as executor:
+        future_to_wav_file = {
+            executor.submit(
+                load_audio_and_proc, fs_r, fs_linreg, fs_cnn, H, n_subbands, wav_file
+            ): wav_file
+            for wav_file in wav_files
+        }
+
+        for future in concurrent.futures.as_completed(future_to_wav_file):
+            wav_file = future_to_wav_file[future]
+            try:
+                data = future.result()
+                audio_data[wav_file.split("/")[-1].split(".")[0]] = data
+            except Exception as exc:
+                print("{} generated an exception: {}".format(wav_file, exc))
+
+    # Save audio data
+    sio.savemat("audio_data.mat", audio_data)
--- EEG_Analysis/preproc_eeg.py
+++ EEG_Analysis/preproc_eeg.py
@@ -1,27 +1,73 @@
+import psutil
 import glob
 import numpy as np
-import scipy
+import scipy.io as sio
+from scipy import signal
+from scipy.signal import resample, butter
+
+from concurrent.futures import ProcessPoolExecutor
+
 
 def load_eeg_filenames(path):
     """
     Loads EEG filenames from a path.
     """
-    filenames = glob.glob(path+"/*.mat")
-    print('Found {0} mat files in {1}'.format(len(filenames), path))
+    filenames = glob.glob(path + "/*.mat")
+    print("Found {0} mat files in {1}".format(len(filenames), path))
     return filenames
 
-def load_eeg_mat(subject_id):
+
+def load_and_proc_eeg(subject_id, fs_linreg=20, fs_cnn=70):
     """
     Loads EEG data from a mat file.
     """
-    eeg_filenames = load_eeg_filenames("./dataset/EEG/{:0>3d}".format(subject_id))
-    eeg_data = np.ndarray((0,0))
-    for matfile in eeg_filenames:
-        mat_struct = scipy.io.loadmat(matfile)
-        eeg_data = mat_struct['trial']['RawData'][0,0]['EegData'][0,0]
-    return eeg_data
+    print("Processing subject {:0>3d}".format(subject_id))
+    eeg_filenames = load_eeg_filenames(
+        "./dataset/EEG/{:0>3d}".format(subject_id))
+    eeg_data_linreg = np.ndarray(shape=(len(eeg_filenames), 1000, 64))
+    eeg_data_cnn = np.ndarray(shape=(len(eeg_filenames), 3500, 64))
 
-if __name__ == '__main__':
-    # Load EEG data
+    for idx, matfile in enumerate(eeg_filenames):
+        mat_struct = sio.loadmat(matfile)
+
+        eeg_data = mat_struct["trial"]["RawData"][0, 0]["EegData"][0, 0]
+        eeg_fs = mat_struct["trial"]["FileHeader"][0,
+                                                   0]["SampleRate"][0, 0][0, 0]
+
+        # Resample EEG data
+        eeg_raw_linreg = resample(
+            eeg_data, eeg_data.shape[0] // eeg_fs * fs_linreg, axis=0
+        )
+        eeg_raw_cnn = resample(
+            eeg_data, eeg_data.shape[0] // eeg_fs * fs_cnn, axis=0)
+
+        # Bandpass filter EEG data: 4-th order butterworth filter
+        sos_linreg = butter(
+            4, [1, 9], btype="bandpass", fs=fs_linreg, analog=False, output="sos"
+        )
+        sos_cnn = butter(
+            4, [1, 32], btype="bandpass", fs=fs_cnn, analog=False, output="sos"
+        )
+        eeg_data_linreg_bp = signal.sosfilt(sos_linreg, eeg_raw_linreg, axis=0)
+        eeg_data_cnn_bp = signal.sosfilt(sos_cnn, eeg_raw_cnn, axis=0)
+
+        eeg_data_linreg[idx, :, :] = eeg_data_linreg_bp
+        eeg_data_cnn[idx, :, :] = eeg_data_cnn_bp
+
+    return eeg_data_linreg, eeg_data_cnn
+
+
+if __name__ == "__main__":
+    # Create dictionary for EEG data
+    eeg_data = {}
     for subject_id in range(1, 38):
-        load_eeg_mat(subject_id)
+        # Load and process EEG data
+        eeg_data_linreg, eeg_data_cnn = load_and_proc_eeg(subject_id)
+        eeg_data["S{:0>3d}".format(subject_id)] = {
+            "linreg": eeg_data_linreg,
+            "cnn": eeg_data_cnn,
+        }
+
+    # Save EEG data
+    print("Saving EEG data to disk...")
+    sio.savemat("eeg_data.mat", eeg_data)
